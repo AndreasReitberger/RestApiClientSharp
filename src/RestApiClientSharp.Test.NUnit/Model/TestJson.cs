af8f6e5af7b3bd9a8d3b0c17457022588db7bb61
Andreas <andy.reitberger@gmail.com>
2025-01-30 12:12:09 +0100

    Send `RestApiError` if request did not succeed
    Fixed #30

---
+++ src/RestApiClientSharp.Test.NUnit/Model/TestJson.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace RestApiClientSharp.Test.NUnit.Model
+{
+    public partial class TestJson
+    {
+        [JsonProperty("userId")]
+        public long UserId { get; set; }
+
+        [JsonProperty("id")]
+        public long Id { get; set; }
+
+        [JsonProperty("title")]
+        public string? Title { get; set; }
+
+        [JsonProperty("completed")]
+        public bool Completed { get; set; }
+    }
+}
